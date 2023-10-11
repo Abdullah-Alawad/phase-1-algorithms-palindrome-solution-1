c81194530e345215f63f8b2571d32d5491104974
Abdullah Alawad <abdullahalawad123321@gmail.com>
2023-10-11 16:22:52 +0300

    Palindrome is Done

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const chars = word.split('');
+  for(let first = 0, last = chars.length - 1; last > first; first++, last--){
+    if(chars[first] !== chars[last])
+      return false;
+  }
+  return true;
 }
 
 /* 
@@ -12,9 +17,28 @@ function isPalindrome(word) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
